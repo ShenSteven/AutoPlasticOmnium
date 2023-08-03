0606ffb4a0d797cd2a6435dc4e0626a3eacac536
hongliang shen <394109529@qq.com>
2023-08-03 15:26:06 +0800

    RUNIN station_name

--- runin/rmainform.py
+++ runin/rmainform.py
@@ -55,7 +55,7 @@ class LoginWind:
                 return
 
             self.ui.lineEdit.Enabled = False
-            gv.cf.station.station_name = self.FixtureNumber[0:self.FixtureNumber.index('-')]
+            # gv.cf.station.station_name = self.FixtureNumber[0:self.FixtureNumber.index('-')]
             gv.cf.station.station_no = self.FixtureNumber + self.AbFace
             self.runinWin = RuninMainForm()
             self.runinWin.show()
